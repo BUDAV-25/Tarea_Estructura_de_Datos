b2b371870b8780169e7d11897d13f37b5a101251
BUDAV <bennydelamparo@gmail.com>
2025-10-07 22:16:27 -0400

    Promedio de los Estudiantes y Almacenar y procesar datos en una matriz

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace AlmacenarProcesar
+{
+    public class AlmacenarProcesar
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de filas: ");
+            int filas = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el número de columnas: ");
+            int columnas = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matriz = new int[filas, columnas];
+
+            Console.WriteLine("\n--- Ingrese los valores de la matriz ---");
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write($"Elemento [{i},{j}]: ");
+                    matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\n--+----- MATRIZ INGRESADA --------");
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int max = matriz[0, 0];
+            int min = matriz[0, 0];
+            int filaMax = 0, colMax = 0;
+            int filaMin = 0, colMin = 0;
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    if (matriz[i, j] > max)
+                    {
+                        max = matriz[i, j];
+                        filaMax = i;
+                        colMax = j;
+                    }
+                    if (matriz[i, j] < min)
+                    {
+                        min = matriz[i, j];
+                        filaMin = i;
+                        colMin = j;
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("RESULTADOS");
+            Console.WriteLine($"Valor máximo: {max} (posición [{filaMax}, {colMax}])");
+            Console.WriteLine($"Valor mínimo: {min} (posición [{filaMin}, {colMin}])");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PromedioEstudiantes/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace PromedioEstudiantes
+{
+    public class PromedioEstudiantes
+    {
+        static void Main(string[] args)
+        {
+            double[,] notas = new double[4, 3];
+            double promedioEstudiante, promedioAsignatura;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Ingrese las 3 notas del estudiante {i + 1}:");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"Nota {j + 1}: ");
+                    notas[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("PROMEDIO DE CADA ESTUDIANTE:");
+            for (int i = 0; i < 4; i++)
+            {
+                promedioEstudiante = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    promedioEstudiante += notas[i, j];
+                }
+                promedioEstudiante /= 3;
+                Console.WriteLine($"Estudiante {i + 1}: {promedioEstudiante:F2}");
+            }
+
+            Console.WriteLine("\nPROMEDIO DE CADA ASIGNATURA:");
+            for (int j = 0; j < 3; j++)
+            {
+                promedioAsignatura = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    promedioAsignatura += notas[i, j];
+                }
+                promedioAsignatura /= 4;
+                Console.WriteLine($"Asignatura {j + 1}: {promedioAsignatura:F2}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
